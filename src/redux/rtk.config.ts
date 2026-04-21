90a22363301619967905050c968051bd7170ca96
Pankajkumar34 <pankajgithubnew@gmail.com>
2026-04-21 20:58:51 +0530

    add live url

--- src/redux/rtk.config.ts
+++ src/redux/rtk.config.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/api',
+    baseUrl:"https://callofgirls.vercel.app/api",
+    // baseUrl: 'http://localhost:4000/api',
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('call_accessToken');
       if (token) {
